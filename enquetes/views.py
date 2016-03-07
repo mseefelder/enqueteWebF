be9d7cce1b603c1b84b95ba023de167eff911518
mseefelder <saa.marcos@gmail.com>
2016-03-07 09:58:57 -0300

    Created view drafts.

--- enquetes/views.py
+++ enquetes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-	return HttpResponse("Placeholder");
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list' : latest_question_list,
+	}
+	return render(request, 'enquetes/index.html', context)
+
+def answer(request, question_id):
+	return HttpResponse("Voce esta respondendo a questao %s." % question_id)
 
 # Create your views here.
