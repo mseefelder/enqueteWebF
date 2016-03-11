b47f184240ff78d2710048945c1ae78e462aac9e
mseefelder <saa.marcos@gmail.com>
2016-03-11 18:42:45 -0300

    Voting works, halfway through part 4 of first app tutorial

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -6,7 +6,7 @@ app_name = 'enquetes'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>[0-9]+)/answer/$', views.answer, name='answer'),
-        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-        url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
+from django.core.urlresolvers import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,13 +16,32 @@ def index(request):
 # Create your views here.
 
 def answer(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        return render(request, 'enquetes/answer.html', {'question':question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'enquetes/answer.html', {'question':question})
     #	return HttpResponse("Voce esta respondendo a questao %s." % question_id)
 
 def results(request, question_id):
-        response = "Voce esta vendo os resultados da questao %s."
-        return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'enquetes/results.html', {'question': question})
 
 def vote(request, question_id):
-        return HttpResponse("Voce esta votando na questao %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, NameError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'enquetes/answer.html', {
+            'question': question, 
+            'error_message': "You didn't select a choice", 
+            })
+    else:
+        # Using F to avoid race conditions:
+        # https://docs.djangoproject.com/en/1.9/ref/models/expressions/#avoiding-race-conditions-using-f
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # If the object needs to be used again, we need to:
+        # selected_choice.refresh_from_db()
+
+        #After successful operation with POST, return HttpResposeRedirect
+        #This will prevent double-voting
+        return HttpResponseRedirect(reverse('enquetes:results', args=(question.id,)))    
